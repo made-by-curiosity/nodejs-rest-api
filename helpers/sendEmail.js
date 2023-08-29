1cd31b944efeded5543fc81098e88525fc1f8c08
TurtlePower <agnkvdm@gmail.com>
2023-08-29 11:44:23 +0300

    add mailgun email sending

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,40 +1,30 @@
-const EMAIL_SENDER = 'qwerty@gmail.com';
+const formData = require('form-data');
+const Mailgun = require('mailgun.js');
+const mailgun = new Mailgun(formData);
+
+require('dotenv').config();
+const { MAILGUN_KEY } = process.env;
+
+const mg = mailgun.client({
+  username: 'api',
+  key: MAILGUN_KEY,
+});
+
+const EMAIL_SENDER =
+  'Mailgun Sandbox <postmaster@sandbox77c0a957f6754edd82fc94e55a321743.mailgun.org>';
 
 const sendEmail = async data => {
-  const email = { ...data, from: EMAIL_SENDER };
+  const { to, subject, html } = data;
 
-  console.log(email);
+  mg.messages
+    .create('sandbox77c0a957f6754edd82fc94e55a321743.mailgun.org', {
+      from: EMAIL_SENDER,
+      to,
+      subject,
+      html,
+    })
+    .then(msg => console.log(msg))
+    .catch(err => console.log(err));
 };
 
 module.exports = sendEmail;
-
-// ===========================================================
-// const nodemailer = require('nodemailer');
-// require('dotenv').config();
-// const { META_PASSWORD } = process.env;
-
-// /* порты 23 465 и 225 */
-
-// const nodemailerConfig = {
-//   host: 'smtp.meta.ua',
-//   port: 465,
-//   secure: true,
-//   auth: {
-//     user: 'qwerty@mail.com',
-//     pass: META_PASSWORD,
-//   },
-// };
-
-// const transporter = nodemailer.createTransport(nodemailer);
-
-// const email = {
-//   to: '',
-//   from: '',
-//   subject: '',
-//   html: '',
-// };
-
-// transporter
-//   .sendMail(email)
-//   .then(() => console.log('mail has been sent'))
-//   .catch(e => console.log(e.message));
